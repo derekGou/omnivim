84f1e5809afebfae036faf8560c45cbcadd05cc4
derekGou <derekgou19@gmail.com>
2025-02-22 14:33:38 -0500

    images

--- main.py
+++ main.py
@@ -5,22 +5,35 @@ from threading import Thread
 from platform import system
 import os
 
-from Windows_Mouse_Movments.main_windows import run_windows
+# from Windows_Mouse_Movments.main_windows import run_windows
 
 def load_image():
-    return Image.open("omni.png")
-
+    with open("mode.txt", "w") as f:
+        typ = f.read()
+        if len(typ) == 0:
+            return Image.open("omnivim.png")
+        if typ == "Normal":
+            return Image.open("omnivimn.png")
+        if typ == "Visual":
+            return Image.open("omnivimv.png")
+        if typ == "Insert":
+            return Image.open("omnivimi.png")
+        if typ == "Mouse":
+            return Image.open("omnivimm.png")
+        
 def run_code():
     if os.name=='nt':
         with open("mode.txt", "w") as f:
             f.truncate(0)
             f.write("windows")
-            t1 = Thread(target=run_windows)
+            # t1 = Thread(target=run_windows)
+            f.close()
     elif os.name=='posix':
         with open("mode.txt", "w") as f:
             f.truncate(0)
             f.write("mac")
             # t2 = Thread(target=holo_touch)
+            f.close()
 
 run_code()
 
