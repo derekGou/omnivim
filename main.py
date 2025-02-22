b71bdd7b1861bf0b154ea62b2df683971b794dcb
derekGou <derekgou19@gmail.com>
2025-02-22 11:17:11 -0500

    main setup

--- main.py
+++ main.py
@@ -3,13 +3,27 @@ from pystray import MenuItem as item
 from PIL import Image, ImageDraw
 from threading import Thread
 from platform import system
+import os
 
 def load_image():
     return Image.open("omni.png")
+print("working1")
 
-def on_clicked(icon, item):
-    if item == "Option 1":
-        pass
+def run_code():
+    if os.name=='nt':
+        with open("mode.txt", "w") as f:
+            f.truncate(0)
+            f.write("windows\n")
+            f.write("normal")
+            # t1 = Thread(target=holo_touch)
+    elif os.name=='posix':
+        with open("mode.txt", "w") as f:
+            f.truncate(0)
+            f.write("mac\n")
+            f.write("normal")
+            # t2 = Thread(target=holo_touch)
+
+run_code()
 
 icon = pystray.Icon("test_icon")
 icon.icon = load_image()
