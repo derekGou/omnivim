fa7e0d24b75389bb9eaecc0b7940ba49e6f4363b
Amit-Weis <amitweisgor@gmail.com>
2025-02-22 18:04:52 -0500

    dnael is goated

--- main.py
+++ main.py
@@ -8,40 +8,29 @@ import os
 from Windows_Mouse_Movments.main_windows import on_key_event as run_windows
 
 def load_image():
-    print("amit not gay")
     with open("vimmode.txt", "r") as f:
-        typ = f.read()
-        print("amit gay")
-        if typ == "Normal":
-            print('a')
-            return Image.open("omnivimn.png")
-        if typ == "Visual":
-            print('b')
-            return Image.open("omnivimv.png")
-        if typ == "Insert":
-            print('c')
-            return Image.open("omnivimi.png")
-        if typ == "Mouse":
-            print('d')
-            return Image.open("omnivimm.png")
-        print('working') 
-        return Image.open("omnivim.png")
-        
-def run_code():
-    if os.name=='nt':
-        t1 = Thread(target=run_windows)
-    elif os.name=='posix':
-        pass
-        # t2 = Thread(target=holo_touch)
+        typ = f.read().strip()
 
-print("daniel not agy")
+    images = {
+        "Normal": "omnivimn.png",
+        "Visual": "omnivimv.png",
+        "Insert": "omnivimi.png",
+        "Mouse": "omnivimm.png"
+    }
+    return Image.open(images.get(typ, "omnivim.png"))
+
+def run_code():
+    if os.name == 'nt':
+        t1 = Thread(target=run_windows)
+        t1.start()
+
 menu = pystray.Menu(
     Item("Toggle State", run_code, default=True) 
 )
 
-icon = pystray.Icon("omnivim", icon=load_image(), menu=menu)
+icon = pystray.Icon("omnivim", load_image(), menu=menu)
 
+def setup(icon):
+    icon.visible = True
 
-icon.icon = load_image()
-icon.visible = True
-icon.run()
+icon.run(setup)
