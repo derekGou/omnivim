47edf32e2f83a82c4944c65999b4d18d545f0927
Daniel <Daniel.li.stuff@gmail.com>
2025-02-22 17:10:37 -0500

    General Bug Fix #3 :eyes:

--- Windows_Mouse_Movments/main_windows.py
+++ Windows_Mouse_Movments/main_windows.py
@@ -6,6 +6,7 @@ from mouse_mode import mouse_on_key_event as mouse
 from visual_mode import visual_on_key_event as visual
 mode = "normal"
 ctrl_mode = False
+shift_mode = False
 def write_mode():
     global mode
     with open("vimmode.txt", "w") as f:
@@ -13,7 +14,7 @@ def write_mode():
         f.write(mode)
     f.close()
 def on_key_event(event):
-    global mode, ctrl_mode
+    global mode, ctrl_mode,shift_mode
     if event.event_type == 'down':
         if mode == "normal":
             match event.name:
@@ -57,12 +58,33 @@ def on_key_event(event):
                 else:
                     mouse(event)
             case "insert":
+                print(event.name)
                 if event.event_type == "down":
-                    keyboard.press(event.name) 
+                    print(event)
+                    if event.name == "ctrl":
+                        keyboard.press(event.name) 
+                        ctrl_mode = True
+                        return False
+                    elif event.name =="shift":
+                        keyboard.press_and_release("shift+q")
+                        keyboard.press_and_release("backspace")
+                        shift_mode = True
+                        return False
+                    elif shift_mode:
+                        keyboard.press_and_release(f"shift + {event.name}")
+                        return False
+                    else:
+                        keyboard.press(event.name)
+                        return False
     elif event.event_type == 'up' and mode == "mouse":
         mouse(event)
         return False
     elif event.event_type == "up":
-        keyboard.release(event.name) 
+        if event.name == "ctrl":
+            keyboard.release(event.name)
+            ctrl_mode = False
+        elif event.name == "shift":
+            keyboard.release(event.name)
+            shift_mode = False
 keyboard.hook(on_key_event, suppress=True)
 keyboard.wait("ctrl+f4")
