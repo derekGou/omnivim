3e6ece83d690bdd435304c5ca0f99efe826ee3ae
Daniel <Daniel.li.stuff@gmail.com>
2025-02-22 10:02:25 -0500

    Windows_Vim_Prototype

---
+++ Windows_Mouse_Movments/main_windows.py
@@ -0,0 +1,33 @@
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Controller, Key
+import keyboard
+def move_left():
+    
+    print('amit gay')# Initialize Mouse and Keyboard
+mouse = Controller()
+kb_controller = Controller()
+
+def on_key_event(event):
+    if event.event_type == 'down':
+        match event.name:
+            case "h":
+                keyboard.press_and_release("left_arrow")
+                return False
+            case "j":
+                keyboard.press_and_release("down_arrow")
+                return False
+            case "k":
+                keyboard.press_and_release("up_arrow")
+                return False
+            case "l":
+                keyboard.press_and_release("right_arrow")
+                return False
+            case "b":
+                keyboard.press("ctrl")
+                keyboard.press_and_release("left_arrow")
+                keyboard.release("ctrl")
+                return False
+
+
+keyboard.hook(on_key_event, suppress=True)
+keyboard.wait("esc")
