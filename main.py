fe2885c8b42ffe3cc434fafc2a3c768274d756df
Amit-Weis <amitweisgor@gmail.com>
2025-02-22 18:25:26 -0500

    two big :fish:

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from PIL import Image
 from threading import Thread
 from platform import system
 import os
-
+import time
 from Windows_Mouse_Movments.main_windows import run_windows
 
 def load_image():
     with open("vimmode.txt", "r") as f:
         typ = f.read().strip()
+    
 
     images = {
-        "Normal": "omnivimn.png",
-        "Visual": "omnivimv.png",
-        "Insert": "omnivimi.png",
-        "Mouse": "omnivimm.png"
+        "normal": "omnivimn.png",
+        "visual": "omnivimv.png",
+        "insert": "omnivimi.png",
+        "mouse": "omnivimm.png"
     }
-    return Image.open(images.get(typ, "omnivim.png"))
+
+    if typ not in images:
+        return Image.open("omnivim.png")
+    return Image.open(images[typ])
 
 def run_code():
     if os.name == 'nt':
@@ -28,9 +32,16 @@ menu = pystray.Menu(
     Item("Toggle State", run_code, default=True) 
 )
 
+def update_image():
+    while True:
+        icon.icon = load_image()
+        time.sleep(0.5)
+    
 icon = pystray.Icon("omnivim", load_image(), menu=menu)
 
 def setup(icon):
     icon.visible = True
 
 icon.run(setup)
+thread = Thread(target = update_image)
+thread.start()
