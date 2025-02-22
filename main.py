f36086f5b7889fc26b096971137982b9ab05ef3f
Amit-Weis <amitweisgor@gmail.com>
2025-02-22 18:59:31 -0500

    finally fixed the stupid thing

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from PIL import Image
 from threading import Thread
 from platform import system
 import os
-import time
 from Windows_Mouse_Movments.main_windows import run_windows
+import time
 
 def load_image():
     with open("vimmode.txt", "r") as f:
         typ = f.read().strip()
-    
+
 
     images = {
         "normal": "omnivimn.png",
@@ -18,30 +18,28 @@ def load_image():
         "insert": "omnivimi.png",
         "mouse": "omnivimm.png"
     }
-    
+
     if typ not in images:
         return Image.open("omnivim.png")
     return Image.open(images[typ])
 
 def run_code():
     if os.name == 'nt':
-        t1 = Thread(target=run_windows)
+        t1 = Thread(target=run_windows, daemon=True)
         t1.start()
 
 menu = pystray.Menu(
-    Item("Toggle State", run_code, default=True) 
+    Item("Toggle State", run_code, default=True)
 )
 
-def update_image():
-    while True:
-        icon.icon = load_image()
-        time.sleep(0.5)
-    
 icon = pystray.Icon("omnivim", load_image(), menu=menu)
 
 def setup(icon):
     icon.visible = True
+    while icon.visible:  # Ensure the thread stops if the icon is closed
+        icon.icon = load_image()
+        print("hello")
+        time.sleep(1)  # Avoid 100% CPU usage
+
 
 icon.run(setup)
-thread = Thread(target = update_image)
-thread.start()
